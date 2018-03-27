95f18aacf25bfb1dca11f20d9166038b90f5d653
fd8783 <30581760+fd8783@users.noreply.github.com>
2018-03-27 21:47:49 +0800

    d

--- Y2528V_GetInToMetro/Assets/Script/interface/interactable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-interface interactable {
-    
-    void Interact(Transform interacter); //who is interacting with this ai
-
-}
